2d58b5e97c49608a46c823a4ac1dbf7c918fce25
Claes Östlund <claes@stenbergit.se>
2022-01-28 13:23:23 +0100

    1.0.0

---
+++ PluginArrayCsv.php
@@ -0,0 +1,24 @@
+<?php
+class PluginArrayCsv{
+  public function array_to_csv($array){
+    $csv = null;
+    foreach ($array as $value) {
+      foreach ($value as $value2) {
+        /**
+         * Replace '"'.
+         */
+        $str = str_replace('"', '""', $value2);
+        /**
+         * Set ...
+         */
+        $str = '"'.$str.'"';
+        /**
+         * Finaly to csv.
+         */
+        $csv .= $str.';';
+      }
+      $csv .= "\n";
+    }
+    return  $csv;
+  }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# Buto-Plugin-ArrayCsv
+Convert array to csv string.
+
+## Usage
+
+```
+wfPlugin::includeonce('array/csv');
+$array_csv = new PluginArrayCsv();
+echo $array_csv->array_to_csv(array('John', 'James'));
+```
